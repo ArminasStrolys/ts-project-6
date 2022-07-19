7fd217b0f3dfd31e788b5769cf0c35b2048500c6
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-07-19 17:22:25 +0300

    Fixing up the useRef with the transpiling

--- src/components/codeArea/CodeArea.tsx
+++ src/components/codeArea/CodeArea.tsx
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useEffect, useRef } from 'react';
+import { useState } from 'react';
 
 export default function CodeArea() {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+  const ref = useRef<any>();
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: './esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
 
   const handleClick = () => {
-    console.log(input);
+    if (!ref.current) {
+      return;
+    }
+    console.log(ref.current);
   };
 
   return (
